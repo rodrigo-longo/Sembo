8aca4e2bba82b44bb0cac87c576d77e4c76615b2
Rodrigo Longo <rodri@MacBook-Pro-de-Rodrigo.local>
2021-02-20 19:08:58 +0100

    I start codeing when the lockdown starts

---
+++ main.js
@@ -0,0 +1,8 @@
+const url = 'http://localhost:3000/'; //URL FOR FETCH
+
+
+// QUERRY SELECTORS 
+const form$$ = document.querySelector('#form') 
+const button$$ = document.querySelector('#button')
+const input$$ = document.querySelector('#input')
+const result$$ = document.querySelector('.results')
