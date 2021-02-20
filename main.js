ad72cec1a7ba9d33f329316817ba8f2f8aafd169
Rodrigo Longo <rodri@MacBook-Pro-de-Rodrigo.local>
2021-02-20 19:19:21 +0100

    And I love the way I'm doing the bootcamp, I'm so happy with my desition

--- main.js
+++ main.js
@@ -109,4 +109,33 @@ button$$.addEventListener('click', () => {
         result$$.appendChild(pTop3$$);
     })
     //ITALY
+    //FRANCE
+    fetch(url+'FR').then(res => res.json()).then(res => {
+        const sortByScore = res.sort((a,b) => {return b.score - a.score;});
+        const totalScore = sortByScore.reduce((sum , scores) => scores.score ?  sum + scores.score: sum , 0);
+        const averageScore = totalScore / res.length;
+        const topScore = sortByScore.slice(0,3);
+        console.log(topScore)
+
+        const pTop3$$ = document.createElement('p');
+        
+        
+        pTop3$$.innerHTML = `
+        <h3>Top 3 Hotels From France (fr):</h3> 
+        · ${topScore[0].name}: score:${topScore[0].score}, <br>
+        · ${topScore[1].name}: score:${topScore[1].score}, <br>
+        · ${topScore[2].name}: score:${topScore[2].score}. <br>
+        <br>
+        Average Score of France (fr): ${averageScore} `
+        
+        const removeP$$ = document.createElement('button');
+            removeP$$.classList.add('btn')
+            removeP$$.innerHTML = 'X'
+            removeP$$.addEventListener('click', () => {pTop3$$.remove()})
+        
+        pTop3$$.appendChild(removeP$$);
+        result$$.appendChild(pTop3$$);
+    })
+    //FRANCE
+})
     
